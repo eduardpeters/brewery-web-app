3aa95c2e0536faa874063ee071190af078ca8e15
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-06-22 20:02:48 +0200

    Initialize project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+
+    return (
+        <div className='app__container'>
+            <h1>A Blank Slate</h1>
+            <p>
+                Have fun coding!
+            </p>
+        </div>
+    );
+}
+
+export default App;
