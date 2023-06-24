6b6fee9da89c33df6f1a9cc93ee6092e99461a58
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-06-24 20:45:48 +0200

    Style Profile Dialog

--- src/components/BeerProfile.jsx
+++ src/components/BeerProfile.jsx
@@ -1,17 +1,38 @@
+import '../styles/BeerProfile.css';
+
 function BeerProfile({ beer, setSelection, dialogElement }) {
 
     function handleClose() {
         setSelection({});
         dialogElement.close();
     }
-
+    console.log(beer);
     return (
-        <div>
-            <h1>PROFILE</h1>
-            <h3 className='beer__name'>{beer.name}</h3>
-            <p className='beer__tagline'>{beer.tagline}</p>
-            <button onClick={handleClose}>Close</button>
-        </div>
+        <main className='profile__container'>
+            <h1 className='profile__name'>{beer.name}</h1>
+            <div className='profile__default'>
+                <img
+                    className='profile_image'
+                    src={beer.image_url}
+                    alt={`A ${beer.name} beer`}
+                />
+                <div className='default__texts'>
+                    <p className='profile__tagline'>{beer.tagline}</p>
+                    <div className='profile__horizontal'>
+                        <p className='profile__attribute'>
+                            <span className='attribute__name'>ABV - </span>{beer.abv}%
+                        </p>
+                        <p className='profile__attribute'>
+                            <span className='attribute__name'>IBU - </span>{beer.ibu}
+                        </p>
+                    </div>
+                    <p className='profile__attribute'>
+                        <span className='attribute__name'>Description - </span>{beer.description}
+                    </p>
+                </div>
+            </div>
+            <button className='profile__close' onClick={handleClose}>Close</button>
+        </main>
     );
 }
 
