d012aaf26280c537bf1c1664b4b15485ae8c2d4c
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-06-25 10:20:17 +0200

    Add Food Pairings to Profile component

--- src/components/BeerProfile.jsx
+++ src/components/BeerProfile.jsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import '../styles/BeerProfile.css';
+import { CloseIcon, CollapseIcon, ExpandIcon } from './Icons';
 
 function BeerProfile({ beer, setSelection, dialogElement }) {
+    const [showPairings, setShowPairings] = useState(false);
 
     function handleClose() {
+        setShowPairings(false);
         setSelection({});
         dialogElement.close();
     }
-    console.log(beer);
+
     return (
         <main className='profile__container'>
             <h1 className='profile__name'>{beer.name}</h1>
@@ -19,19 +23,41 @@ function BeerProfile({ beer, setSelection, dialogElement }) {
                 <div className='default__texts'>
                     <p className='profile__tagline'>{beer.tagline}</p>
                     <div className='profile__horizontal'>
-                        <p className='profile__attribute'>
-                            <span className='attribute__name'>ABV - </span>{beer.abv}%
+                        <p className='profile__attribute profile__attribute-short'>
+                            <span className='attribute__name'>ABV </span>{beer.abv}%
                         </p>
-                        <p className='profile__attribute'>
-                            <span className='attribute__name'>IBU - </span>{beer.ibu}
+                        <p className='profile__attribute profile__attribute-short'>
+                            <span className='attribute__name'>IBU </span>{beer.ibu}
+                        </p>
+                        <p className='profile__attribute profile__attribute-short'>
+                            <span className='attribute__name'>Since </span>{beer.first_brewed}
                         </p>
                     </div>
                     <p className='profile__attribute'>
                         <span className='attribute__name'>Description - </span>{beer.description}
                     </p>
+                    <button
+                        className='profile__pairings-btn'
+                        onClick={() => setShowPairings(!showPairings)}
+                        title={`${showPairings ? 'Hide' : 'Show'} food pairings`}
+                    >
+                        {showPairings ? <CollapseIcon /> : <ExpandIcon />}
+                        Food pairings
+                    </button>
+                    {showPairings &&
+                        <ul className='pairings__list'>
+                            {beer.food_pairing.map(food => <li key={food}>{food}</li>)}
+                        </ul>}
                 </div>
             </div>
-            <button className='profile__close' onClick={handleClose}>Close</button>
+            <button
+                className='profile__close'
+                onClick={handleClose}
+                title='Close profile'
+            >
+                <CloseIcon />
+                Close
+            </button>
         </main>
     );
 }
