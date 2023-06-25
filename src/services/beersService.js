a16b228ec7fd1115ad91a5338a8fab5a578ee876
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-06-25 15:46:24 +0200

    Introduce pagination query params

--- src/services/beersService.js
+++ src/services/beersService.js
@@ -1,7 +1,15 @@
 const API_BASE_URL = 'https://api.punkapi.com/v2/beers';
+const PAGE_SIZE = 30;
 
-async function getBeers(url = API_BASE_URL) {
-    const response = await fetch(url);
+async function getBeers(url = API_BASE_URL, page = 1) {
+    let requestUrl;
+    if (url === API_BASE_URL) {
+        requestUrl = url + '/?';
+    } else {
+        requestUrl = url + '&';
+    }
+    requestUrl = `${requestUrl}page=${page}&per_page=${PAGE_SIZE}`;
+    const response = await fetch(requestUrl);
     if (response.ok) {
         return await response.json();
     } else {
