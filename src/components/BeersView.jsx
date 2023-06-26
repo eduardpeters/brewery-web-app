48c6eb4a76920852cd430f265f171aef63d4afd0
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-06-26 18:44:47 +0200

    Add Structure and logic to BeersView component

--- src/components/BeersView.jsx
+++ src/components/BeersView.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
 import BeersGrid from "./BeersGrid";
+import { GridIcon, TableIcon } from "./Icons";
+import '../styles/BeersView.css';
 
 function BeersView({ beers, setSelection }) {
+    const [gridView, setGridView] = useState(true);
+
     return (
-        <BeersGrid beers={beers} setSelection={setSelection} />
+        <>
+            <button
+                className='view__switcher'
+                onClick={() => setGridView(!gridView)}
+                title={`Change to ${gridView ? 'table' : 'grid'} view`}
+            >
+                {gridView ? <TableIcon /> : <GridIcon />}
+                <span className='view__switcher-text'>
+                    {`Show in ${gridView ? 'table' : 'grid'} view`}
+                </span>
+            </button>
+            {
+                gridView
+                    ?
+                    <BeersGrid beers={beers} setSelection={setSelection} />
+                    :
+                    <div>Show table view</div>
+            }
+        </>
     );
 }
 
